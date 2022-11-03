41f7d445a9f1f3c963204b371bfacb7d17830d41
Niels Mauel <niels@rebelit.nl>
2022-11-03 15:29:43 +0100

    Added initial workspace to exercise!

---
+++ Exercises/Kata1.cs
@@ -0,0 +1,22 @@
+namespace WorkshopTDD;
+
+/// ----------------------------------
+/// FizzBuzz
+/// ----------------------------------
+/// FizzBuzz is one of the most famous coding exercises for beginners.
+/// 
+/// Requirements:
+/// 
+/// 1. Write a “FizzBuzz” method that accepts a number as input and returns it as a string.
+/// 
+/// 2. For multiples of three return “Fizz” instead of the number.
+/// 
+/// 3. For the multiples of five return “Buzz”.
+/// 
+/// 4. For numbers that are multiples of both three and five return “FizzBuzz”.
+/// 
+/// </summary>
+public class Kata1
+{
+
+}
---
+++ Exercises/Kata2.cs
@@ -0,0 +1,29 @@
+﻿namespace WorkshopTDD;
+
+/// <summary>
+/// ----------------------------------
+/// String Calculator
+/// ----------------------------------
+/// Create a simple calculator that takes a string and returns an integer.
+/// 
+/// Requirements:
+/// 
+/// 1. The method can take up to two numbers, separated by commas, and will return their sum as a result.
+///         So the inputs can be: “”, “1”, “1,2”. And for an empty string, it will return 0.
+///         
+/// 2. Allow the add method to handle an unknown number of arguments.
+/// 
+/// 3. Allow the add method to handle newlines as separators, instead of commas.
+/// 
+/// 4. Add validation to not to allow a separator at the end.
+/// 
+/// 5. Allow the add method to handle different delimiters.
+/// 
+/// 6. To change the delimiter, the beginning of the input will contain a separate line that looks like this:  
+///         //[delimiter]\n[numbers] Numbers bigger than 1000 should be ignored, so adding 2 + 1001 = 2
+///         
+/// </summary>
+public class Kata2
+{
+
+}
